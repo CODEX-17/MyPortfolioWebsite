1cb198ee40da0beb96523635c57d8ce8cd049603
CODEX-17 <112467114+CODEX-17@users.noreply.github.com>
2025-03-19 10:21:11 +0800

    fix some not responsive add more thumbnail in projects highlights

--- my-portfolio/src/sections/Projects/Projects.jsx
+++ my-portfolio/src/sections/Projects/Projects.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Projects.css'
 import { FaReact } from "react-icons/fa";
-import { motion } from 'framer-motion';
-import eSchoolLoginPic from '../../assets/images/e-schoolapp-login.jpg'
+import { motion } from 'framer-motion'
 import { SiExpress } from "react-icons/si";
 import { TbBrandMysql } from "react-icons/tb";
 import { GrLinkNext } from "react-icons/gr";
 import { COLORS } from '../../constants/colors';
-import { FaEye } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
+import eSchoolLoginPic from '../../assets/images/e-schoolApp-thumbnail.jpg'
+import scholarshipPic from '../../assets/images/scholarship-thumbnail.jpg'
+import NFRDIPic from '../../assets/images/nfrdi-thumbnail.jpg'
+
 
 const Projects = ({ projectsRef, darkMode }) => {
 
@@ -39,14 +41,13 @@ const Projects = ({ projectsRef, darkMode }) => {
                     <div 
                         className='image-container'
                     >
-                        <motion.img 
+                        <img 
                             initial={{ opacity: 1, zIndex: 4 }}
                             whileHover={{ opacity: 0.5, zIndex: 0 }}
                             src={eSchoolLoginPic} 
                             alt="thumbnail" 
                             className='image-thumbnail'
                         />
-                        <FaEye size={30} style={{ position: 'absolute', zIndex: 2 }}/>
                     </div>
                     <div className='projects-card-details'>
                         <div className='projects-card-technology-list'>
@@ -70,19 +71,20 @@ const Projects = ({ projectsRef, darkMode }) => {
                             </div>
                         </div>
                         <div className='projects-card-description'>
-                        <div className='d-flex flex-column'>
-                            <b>Description:</b>
-                            <p> A web application that communication and collaboration platform for student and teacher.</p>
-                        </div>
-                        <div className='d-flex flex-column'>
-                            <b>Key Features:</b>
-                            <p>Create Classes, Posting of assignments and Activities, Posting of learning materials,  Create Quiz, Real-time Chat.</p>
-                        </div>
-                        </div>
-                        <div className='d-flex w-100 mt-2'>
-                        <button>Repository</button>
-                        </div>
+                            <div className='d-flex flex-column'>
+                                <b>Description:</b>
+                                <p> A web application that communication and collaboration platform for student and teacher.</p>
+                            </div>
+                            <div className='d-flex flex-column'>
+                                <b>Key Features:</b>
+                                <p>Create Classes, Posting of assignments and Activities, Posting of learning materials,  Create Quiz, Real-time Chat.</p>
+                            </div>
+                        </div> 
+                        <div className='flex-1 d-flex w-100 mt-2'>
+                            <button>Repository</button>
+                        </div>                       
                     </div>
+                    
                 </motion.div>
 
                 <motion.div 
@@ -93,37 +95,48 @@ const Projects = ({ projectsRef, darkMode }) => {
                     className='projects-card'
                 >
                     <div className='projects-card-head'>
-                        <h3>E-SchoolApp</h3>
+                        <h3>Scholarhip Management System</h3>
                         <p>(Web Application)</p>
                     </div>
                     <div className='image-container'>
-                        <img src={eSchoolLoginPic} alt="thumbnail" className='image-thumbnail'/>
+                        <img src={scholarshipPic} alt="thumbnail" className='image-thumbnail'/>
                     </div>
                     <div className='projects-card-details'>
                         <div className='projects-card-technology-list'>
-                        <div className='technology-card'>
-                            <FaReact/>
-                            React
-                        </div>
-                        <div className='technology-card'>
-                            <FaReact/>
-                            React
-                        </div>
+                            <div className='technology-card'>
+                                <FaReact/>
+                                React
+                            </div>
+                            <div 
+                                className='technology-card'
+                                style={{ backgroundColor: 'green', color: '#fff' }}
+                            >
+                                <SiExpress/>
+                                Express
+                            </div>
+                            <div 
+                                className='technology-card'
+                                style={{ backgroundColor: '#005984', color: '#fff' }}
+                            >
+                                <TbBrandMysql/>
+                                MySQL
+                            </div>
                         </div>
                         <div className='projects-card-description'>
-                        <div className='d-flex flex-column'>
-                            <b>Description:</b>
-                            <p> A web application that communication and collaboration platform for student and teacher.</p>
+                            <div className='d-flex flex-column'>
+                                <b>Description:</b>
+                                <p>A web app that simplifies scholarship applications and management for students and administrators.</p>
+                            </div>
+                            <div className='d-flex flex-column'>
+                                <b>Key Features:</b>
+                                <p>Account creation, admin posting and updates, and a dashboard for monitoring scholarships, available slots, and program details.</p>
+                            </div>
                         </div>
-                        <div className='d-flex flex-column'>
-                            <b>Key Features:</b>
-                            <p>Create Classes, Posting of assignments and Activities, Posting of learning materials,  Create Quiz, Real-time Chat.</p>
-                        </div>
-                        </div>
-                        <div className='d-flex w-100 mt-2'>
-                        <button>Repository</button>
+                        <div className='d-flex w-100 mt-2 bg-info'>
+                            <button>Repository</button>
                         </div>
                     </div>
+                    
                 </motion.div>
 
                 <motion.div 
@@ -134,37 +147,48 @@ const Projects = ({ projectsRef, darkMode }) => {
                     className='projects-card'
                 >
                     <div className='projects-card-head'>
-                        <h3>E-SchoolApp</h3>
+                        <h3>NFRDI Procurement Dashboard</h3>
                         <p>(Web Application)</p>
                     </div>
                     <div className='image-container'>
-                        <img src={eSchoolLoginPic} alt="thumbnail" className='image-thumbnail'/>
+                        <img src={NFRDIPic} alt="thumbnail" className='image-thumbnail'/>
                     </div>
                     <div className='projects-card-details'>
                         <div className='projects-card-technology-list'>
-                        <div className='technology-card'>
-                            <FaReact/>
-                            React
-                        </div>
-                        <div className='technology-card'>
-                            <FaReact/>
-                            React
-                        </div>
+                            <div className='technology-card'>
+                                <FaReact/>
+                                React
+                            </div>
+                            <div 
+                                className='technology-card'
+                                style={{ backgroundColor: 'green', color: '#fff' }}
+                            >
+                                <SiExpress/>
+                                Express
+                            </div>
+                            <div 
+                                className='technology-card'
+                                style={{ backgroundColor: '#005984', color: '#fff' }}
+                            >
+                                <TbBrandMysql/>
+                                MySQL
+                            </div>
                         </div>
                         <div className='projects-card-description'>
-                        <div className='d-flex flex-column'>
-                            <b>Description:</b>
-                            <p> A web application that communication and collaboration platform for student and teacher.</p>
+                            <div className='d-flex flex-column'>
+                                <b>Description:</b>
+                                <p>A web app that streamlines NFRDI's procurement process with transparency, efficiency, and real-time tracking.</p>
+                            </div>
+                            <div className='d-flex flex-column'>
+                                <b>Key Features:</b>
+                                <p>Manages procurement data, bidding, and alternative methods, providing an overview with options to add, edit, and delete records.</p>
+                            </div>
                         </div>
-                        <div className='d-flex flex-column'>
-                            <b>Key Features:</b>
-                            <p>Create Classes, Posting of assignments and Activities, Posting of learning materials,  Create Quiz, Real-time Chat.</p>
-                        </div>
-                        </div>
-                        <div className='d-flex w-100 mt-2'>
-                        <button>Repository</button>
+                        <div className='flex-1 d-flex w-100 mt-2'>
+                            <button>Repository</button>
                         </div>
                     </div>
+                    
                 </motion.div>
             </div>
 
