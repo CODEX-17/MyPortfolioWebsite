bd023b3c581f59980d84e0bee05a7f4925ad6d0e
CODEX-17 <112467114+CODEX-17@users.noreply.github.com>
2025-04-25 17:14:08 +0800

    add animation in tech-section

--- my-portfolio/src/assets/imagePath.ts
+++ my-portfolio/src/assets/imagePath.ts
@@ -1,9 +1,41 @@
 import logoDefault from '../assets/images/rp.png';
 import logoWhite from '../assets/images/rp-white.png'
 import rumarPicture from './images/rumar-green-bg.png'
+import reactIcon from '../assets/images/react-icon.png'
+import PHPIcon from '../assets/images/php-icons.png'
+import JSIcon from '../assets/images/javascript-icons.png'
+import ExpoIcon from '../assets/images/expo-icons.png'
+import NodeIcon from '../assets/images/node-icons.png'
+import ViteIcon from '../assets/images/vite-icons.png'
+import BootstrapIcon from '../assets/images/bootstrap-icons.png'
+import TailwindIcon from '../assets/images/tailwind-icons.png'
+import ReactNativeIcon from '../assets/images/react-native-icons.png'
+import MySQLIcon from '../assets/images/mysql-icons.png'
+import JavaIcon from '../assets/images/java-icons.png'
+import NPMIcon from '../assets/images/npm-icons.png'
+import IonicIcon from '../assets/images/ionic-icons.png'
+import GitIcon from '../assets/images/git-icons.png'
+import TypeScriptIcon from '../assets/svg/typescript-svgrepo-com.svg'
+import GithubIcon from '../assets/svg/github-svgrepo-com.svg'
 
 export const images = {
     logoDefault,
     logoWhite,
     rumarPicture,
+    reactIcon,
+    PHPIcon,
+    JSIcon,
+    ExpoIcon,
+    NodeIcon,
+    ViteIcon,
+    BootstrapIcon,
+    TailwindIcon,
+    ReactNativeIcon,
+    MySQLIcon,
+    JavaIcon,
+    GithubIcon,
+    NPMIcon,
+    IonicIcon,
+    GitIcon,
+    TypeScriptIcon,
 }
--- my-portfolio/src/sections/Technologies/Tech.tsx
+++ my-portfolio/src/sections/Technologies/Tech.tsx
@@ -1,23 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext, useMemo } from 'react'
 import './Tech.css'
-import reactIcon from '../../assets/images/react-icon.png'
-import PHPIcon from '../../assets/images/php-icons.png'
-import JSIcon from '../../assets/images/javascript-icons.png'
-import ExpoIcon from '../../assets/images/expo-icons.png'
-import NodeIcon from '../../assets/images/node-icons.png'
-import ViteIcon from '../../assets/images/vite-icons.png'
-import BootstrapIcon from '../../assets/images/bootstrap-icons.png'
-import TailwindIcon from '../../assets/images/tailwind-icons.png'
-import ReactNativeIcon from '../../assets/images/react-native-icons.png'
-import MySQLIcon from '../../assets/images/mysql-icons.png'
-import JavaIcon from '../../assets/images/java-icons.png'
-import PSIcon from '../../assets/images/ps-icons.png'
-import NPMIcon from '../../assets/images/npm-icons.png'
-import CanvaIcons from '../../assets/images/canva-icons.png'
-import IonicIcon from '../../assets/images/ionic-icons.png'
-import GitIcon from '../../assets/images/git-icons.png'
 import { COLORS } from '../../constants/colors'
 import { ThemeContext } from '../../../context/ThemeContext'
+import { images } from '../../assets/imagePath'
+import { motion } from 'framer-motion'
 
 const Tech = () => {
 
@@ -26,113 +12,129 @@ const Tech = () => {
 
   const techList = [
     {
-      icon: reactIcon,
+      icon: images.reactIcon,
       name: 'React',
       link: 'https://react.dev/'
     },
     {
-      icon: PHPIcon,
+      icon: images.PHPIcon,
       name: 'PHP',
       link: 'https://www.php.net/'
     },
     {
-      icon: JSIcon, 
+      icon: images.JSIcon, 
       name: 'Javascript',
       link: 'https://www.javascript.com/'
     },
     {
-      icon: ReactNativeIcon, 
+      icon: images.TypeScriptIcon,
+      name: 'Typesscript',
+      link: 'https://www.typescriptlang.org/'
+    },
+    {
+      icon: images.ReactNativeIcon, 
       name: 'React Native',
       link: 'https://reactnative.dev/'
     },
     {
-      icon: ExpoIcon,
+      icon: images.ExpoIcon,
       name: 'Expo',
       link: 'https://expo.dev/'
     },
     {
-      icon: NodeIcon,
+      icon: images.NodeIcon,
       name: 'Node',
       link: 'https://nodejs.org/en'
     },
     {
-      icon: ViteIcon,
+      icon: images.ViteIcon,
       name: 'Vite',
       link: 'https://vite.dev/'
     },
     {
-      icon: BootstrapIcon,
+      icon: images.BootstrapIcon,
       name: 'Bootstrap',
       link: 'https://getbootstrap.com/'
     },
     {
-      icon: TailwindIcon,
+      icon: images.TailwindIcon,
       name: 'Tailwind',
       link: 'https://react.dev/'
     },
     {
-      icon: MySQLIcon,
+      icon: images.MySQLIcon,
       name: 'MySQL',
       link: 'https://www.mysql.com/'
     },
     {
-      icon: PSIcon,
-      name: 'Photoshop',
-      link: 'https://www.adobe.com/express/create/logo'
-    },
-    {
-      icon: CanvaIcons,
-      name: 'Canva',
-      link: 'https://www.adobe.com/express/create/logo'
-    },
-    {
-      icon: JavaIcon,
+      icon: images.JavaIcon,
       name: 'Java',
       link: 'https://www.java.com/en/'
     },
     {
-      icon: NPMIcon,
+      icon: images.NPMIcon,
       name: 'NPM',
       link: 'https://www.java.com/en/'
     },
     {
-      icon: IonicIcon,
+      icon: images.IonicIcon,
       name: 'Ionic',
       link: 'https://www.java.com/en/'
     },
     {
-      icon: GitIcon,
+      icon: images.GithubIcon,
+      name: 'Github',
+      link: 'https://github.com/'
+    },
+    {
+      icon: images.GitIcon,
       name: 'Git',
       link: 'https://www.java.com/en/'
     },
   ]
 
+  const openLink = (link: string) => useCallback(() => window.location.href = link, [link])
+
+  const cardMotionProps = (index: number) => useMemo(() => ({
+    initial: { rotate: 5 },
+    whileInView: { rotate: 0, transition: { delay: parseFloat(`0.${index + 2}`), type: 'spring', stiffness: 300} },
+    whileHover: { rotate: 5 },
+    transition: { type: 'spring', stiffness: 300 }
+  }),[])
+
   return (
     <div className='content-tech-section'>
         <div className='head-tech-section'>
-            <h1 
+            <motion.h1
+              initial={{ opacity: 0 }} 
+              whileInView={{ opacity: 1, transition: { delay: 0.5 }}}
               className='title' 
               style={{ color: themeColors.green }}
-            >Technologies I use.</h1>
+            >Technologies I use.</motion.h1>
         </div>
         <div className='body-tech-section row'>
         {techList.map((data, index) => (
-          <div 
+          <motion.div 
             className="col-lg-3 col-md-4 col-sm-6 col-12 mb-md-2 d-flex align-items-center justify-content-center" 
-            key={index} 
-            onClick={() => window.location.href = data.link}
+            key={index}
+            onClick={() => openLink(data.link)}
+            {...cardMotionProps(index)}
           >
             <div className="card">
               <img src={data.icon} alt={data.name}/>
-              <p className="mt-2 fs-7 fs-sm-4 fs-md-5">{data.name}</p>
+              <p 
+                className="mt-2 fs-7 fs-sm-4 fs-md-5" 
+              >{data.name}</p>
             </div>
-          </div>
+          </motion.div>
         ))}
         <div className='text-center mt-2'>
-          <h1 
+          <motion.h1 
+            initial={{ opacity: 0 }} 
+            whileInView={{ opacity: 1, transition: { delay: 1 }}}
             className='title fs-5'
             style={{ color: themeColors.green }}
-          >...and many others!</h1>
+          >...and many others!</motion.h1>
         </div>
         </div>
     </div>
