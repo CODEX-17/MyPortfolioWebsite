33bacd51af351442f68867ee416786b8ea2f004f
CODEX-17 <112467114+CODEX-17@users.noreply.github.com>
2025-05-09 13:56:17 +0800

    Add View & Repo button in project-page

--- my-portfolio/src/components/Card/Card.tsx
+++ my-portfolio/src/components/Card/Card.tsx
@@ -1,104 +1,135 @@
-import React, { memo, useContext, useState } from 'react'
-import { ThemeContext } from '../../../context/ThemeContext'
-import { COLORS } from '../../constants/colors'
-import './Card.css'
-import { shortenSentence } from '../../utils/shortenWord'
-
+import React, { memo, useContext, useState } from "react";
+import { ThemeContext } from "../../../context/ThemeContext";
+import { COLORS } from "../../constants/colors";
+import "./Card.css";
+import { shortenSentence } from "../../utils/shortenWord";
+import { BiGitRepoForked } from "react-icons/bi";
+import { MdViewCozy } from "react-icons/md";
 
 interface CardPropsType {
-    width?: string,
-    minWidth?: string,
-    maxWidth?: string,
-    minHeight?: string,
-    maxHeight?: string,
-    height?: string,
-    title: string,
-    description: string,
-    thumbnail: any,
-    icon: any,
-    className?: string,
-    style?: React.CSSProperties,
+  width?: string;
+  minWidth?: string;
+  maxWidth?: string;
+  minHeight?: string;
+  maxHeight?: string;
+  height?: string;
+  title: string;
+  description: string;
+  thumbnail: any;
+  icon: any;
+  repository: string;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
+const Card = ({
+  width,
+  minWidth,
+  maxWidth,
+  height,
+  minHeight,
+  maxHeight,
+  title,
+  description,
+  thumbnail,
+  icon,
+  repository,
+  className,
+  style,
+}: CardPropsType) => {
+  const { theme, handleTheme } = useContext(ThemeContext);
+  const themeColors = theme === "dark" ? COLORS.dark : COLORS.light;
+  const [hovered, setHovered] = useState(false);
 
-const Card = ({ 
-     width,
-     minWidth,
-     maxWidth,
-     height,
-     minHeight,
-     maxHeight,
-     title,
-     description,
-     thumbnail, 
-     icon,
-     className,
-     style,
-    } : CardPropsType) => {
+  const screenWidth = window.innerWidth;
 
-  const { theme, handleTheme } = useContext(ThemeContext)
-  const themeColors = theme === 'dark' ? COLORS.dark : COLORS.light
-  const [hovered, setHovered] = useState(false)
-  
-  const screenWidth = window.innerWidth
+  const cardWidth = width ? width : "auto";
+  const cardMinWidth = minWidth
+    ? minWidth
+    : screenWidth > 425
+    ? "400px"
+    : "300px";
+  const cardMaxWidth = maxWidth ? maxWidth : "auto";
 
-  const cardWidth = width ? width : 'auto'
-  const cardMinWidth = minWidth ? minWidth : screenWidth > 425 ? '400px' : '300px'
-  const cardMaxWidth = maxWidth ? maxWidth : 'auto'
-
-  const cardHeight = height ? height : 'auto'
-  const cardMinHeight = minHeight ? minHeight : '200px'
-  const cardMaxHeight = maxHeight ? maxHeight : 'none'
-
+  const cardHeight = height ? height : "auto";
+  const cardMinHeight = minHeight ? minHeight : "200px";
+  const cardMaxHeight = maxHeight ? maxHeight : "none";
 
   return (
-    <div 
-        className={`customize-card ${className}` }
-        style={{ 
-            backgroundColor: themeColors.background,
-            height: cardHeight,
-            minHeight: cardMinHeight,
-            maxHeight: cardMaxHeight,
-            width: cardWidth,
-            minWidth: cardMinWidth,
-            maxWidth: cardMaxWidth,
-            ...(style || {}),
-        }}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        onClick={() => setHovered(!hovered)}
+    <div
+      className={`customize-card ${className}`}
+      style={{
+        backgroundColor: themeColors.background,
+        height: cardHeight,
+        minHeight: cardMinHeight,
+        maxHeight: cardMaxHeight,
+        width: cardWidth,
+        minWidth: cardMinWidth,
+        maxWidth: cardMaxWidth,
+        ...(style || {}),
+      }}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onClick={() => setHovered(!hovered)}
     >
-        <div className={ theme === 'dark' ? 'gradient-effect-dark' : 'gradient-effect-light' }>
+      <div
+        className={
+          theme === "dark" ? "gradient-effect-dark" : "gradient-effect-light"
+        }
+      ></div>
+      <div className="content-image">
+        <div
+          className="content-image-container"
+          style={{
+            filter: hovered ? "blur(0px)" : "blur(1px)",
+            transition: "filter 0.3s ease-in-out",
+          }}
+        >
+          <img
+            src={thumbnail}
+            alt="logo"
+            style={{
+              scale: hovered ? "110%" : "100%",
+              transition: "scale 1s ease-in-out",
+            }}
+          />
         </div>
-        <div className="content-image">
-            <div 
-                className='content-image-container'
-                style={{ 
-                    filter: hovered ? 'blur(0px)' : 'blur(1px)',
-                    transition: 'filter 0.3s ease-in-out'
-                }}
+      </div>
+      <div
+        className="d-flex flex-column w-100 position-absolute bottom-0 p-3 cursor-pointer"
+        style={{ zIndex: 10, fontSize: ".8rem" }}
+      >
+        <img src={icon} alt="logo" className="logo mb-2" />
+        <h4 style={{ color: themeColors.text }}>{title}</h4>
+        <p style={{ color: themeColors.text }}>
+          {shortenSentence(description, hovered ? 100 : 50)}
+        </p>
+        {hovered && (
+          <div className="d-flex gap-2">
+            <button
+              style={{
+                backgroundColor: COLORS.dark.background,
+                color: COLORS.light.background,
+              }}
             >
-                <img 
-                    src={thumbnail} 
-                    alt="logo" 
-                    style={{ 
-                        scale: hovered ? '110%' : '100%',
-                        transition: 'scale 1s ease-in-out'
-                    }}
-                />
-            </div>
-        </div>
-        <div className='d-flex flex-column w-100 position-absolute bottom-0 p-3 cursor-pointer' style={{ zIndex: 10, fontSize: '.8rem' }}>
-            <img 
-                src={icon}
-                alt="logo" 
-                className='logo mb-2'
-            /> 
-            <h4 style={{ color: themeColors.text }}>{title}</h4>
-            <p style={{ color: themeColors.text }}>{shortenSentence(description, hovered ? 100 : 50)}</p>
-        </div>
+              <MdViewCozy size={15} />
+              View
+            </button>
+            <button
+              onClick={() => window.open(repository, "_blank")}
+              style={{
+                backgroundColor: COLORS.dark.background,
+                color: COLORS.light.background,
+              }}
+            >
+              <BiGitRepoForked size={15} />
+              Repository
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default memo(Card)
+export default memo(Card);
--- my-portfolio/src/pages/ProjectPage/ProjectsPage.tsx
+++ my-portfolio/src/pages/ProjectPage/ProjectsPage.tsx
@@ -1,183 +1,334 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react'
-import './ProjectsPage.css'
-import Footer from '../../sections/Footer/Footer'
-import { ThemeContext } from '../../../context/ThemeContext'
-import { COLORS } from '../../constants/colors'
+import React, { useContext, useEffect, useMemo, useState } from "react";
+import "./ProjectsPage.css";
+import Footer from "../../sections/Footer/Footer";
+import { ThemeContext } from "../../../context/ThemeContext";
+import { COLORS } from "../../constants/colors";
 import { motion, LazyMotion, domAnimation } from "framer-motion";
 import { GoHomeFill } from "react-icons/go";
 import { IoMdSunny } from "react-icons/io";
-import { useNavigate } from 'react-router-dom'
-import { 
-  MdPhoneAndroid, 
-  MdComputer, 
-  MdDarkMode, 
+import { useNavigate } from "react-router-dom";
+import {
+  MdPhoneAndroid,
+  MdComputer,
+  MdDarkMode,
   MdOutlineDesignServices,
-  MdOutlinePhonelink 
+  MdOutlinePhonelink,
 } from "react-icons/md";
-import GridMotion from '../../components/GridMotion/GridMotion';
-import Card from '../../components/Card/Card'
-import { projects as projectList } from '../../data/projectList'
-import { images } from '../../assets/imagePath'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-
+import GridMotion from "../../components/GridMotion/GridMotion";
+import Card from "../../components/Card/Card";
+import { projects as projectList } from "../../data/projectList";
+import { images } from "../../assets/imagePath";
+//import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const ProjectsPage = () => {
-
-  const { theme, handleTheme } = useContext(ThemeContext)
-  const themeColors = theme === 'dark' ? COLORS.dark : COLORS.light 
-  const navigate = useNavigate()
+  const { theme, handleTheme } = useContext(ThemeContext);
+  const themeColors = theme === "dark" ? COLORS.dark : COLORS.light;
+  const navigate = useNavigate();
 
   // note: you'll need to make sure the parent container of this component is sized properly
   const items = [
-    
-    <img className='img-display h-100 position-absolute' src={images.eSchoolAppLogo} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.rumarPicture} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.scholarshipPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.NFRDIPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolAppLogo} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.rumarPicture} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.scholarshipPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.NFRDIPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
-    <img className='img-display h-100 position-absolute' src={images.eSchoolLoginPic} alt="" />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolAppLogo}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.rumarPicture}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.scholarshipPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.NFRDIPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolAppLogo}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.rumarPicture}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.scholarshipPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.NFRDIPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
+    <img
+      className="img-display h-100 position-absolute"
+      src={images.eSchoolLoginPic}
+      alt=""
+    />,
   ];
-  
 
   const menuItems = [
     {
-      title: 'All',
-      icon: <MdOutlinePhonelink color={ themeColors.icon } size={20}/>
+      title: "All",
+      icon: <MdOutlinePhonelink color={themeColors.icon} size={20} />,
     },
     {
-      title: 'Website',
-      icon: <MdComputer color={ themeColors.icon } size={20}/>
+      title: "Website",
+      icon: <MdComputer color={themeColors.icon} size={20} />,
     },
     {
-      title: 'Mobile',
-      icon: <MdPhoneAndroid color={ themeColors.icon } size={20}/>
+      title: "Mobile",
+      icon: <MdPhoneAndroid color={themeColors.icon} size={20} />,
     },
     {
-      title: 'Digital Artwork',
-      icon: <MdOutlineDesignServices color={ themeColors.icon } size={20}/>
+      title: "Digital Artwork",
+      icon: <MdOutlineDesignServices color={themeColors.icon} size={20} />,
     },
-  ]
+  ];
 
-  const [selected, setSelected] = useState<string | null>(menuItems[0].title)
+  const [selected, setSelected] = useState<string | null>(menuItems[0].title);
 
   const [width, setWidth] = useState(window.innerWidth);
 
-
   //For Screen Size
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth)
+    const handleResize = () => setWidth(window.innerWidth);
 
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <motion.div 
-      className='projects-content'
-      style={{ backgroundColor: themeColors.background, transition: 'opacity 0.3s ease' }}
+    <motion.div
+      className="projects-content"
+      style={{
+        backgroundColor: themeColors.background,
+        transition: "opacity 0.3s ease",
+      }}
       animate={{ backgroundColor: themeColors.background }}
-      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
     >
       <div className="container">
         <div className="header">
           <div className="d-flex w-100 align-items-center justify-content-between">
-            <GoHomeFill 
-              color={ themeColors.icon } 
+            <GoHomeFill
+              color={themeColors.icon}
               size={25}
-              cursor={'pointer'}
-              onClick={() => navigate('/')}
+              cursor={"pointer"}
+              onClick={() => navigate("/")}
             />
             <h3 style={{ color: themeColors.text }}>My Projects</h3>
             <motion.button
-                initial={{
-                  opacity: 0,
-                  rotate: 120,
-                }}
-                animate={{
-                  opacity: 1,
-                  rotate: 300,
-                  transition: { duration: 1 },
-                }}
-                whileHover={{
-                  opacity: 0.5,
-                  rotate: 120,
-                }}
-                whileTap={{
-                  opacity: 0.5,
-                  rotate: 120,
-                }}
-                transition={{
-                  duration: 0.3,
-                  ease: "easeInOut",
-                }} 
-                style={{ backgroundColor: 'transparent' }}
-                onClick={() => handleTheme(theme === 'dark' ? 'light' : 'dark')}
-                > 
-              {
-                theme === 'dark' ?
-                <IoMdSunny color={themeColors.icon} size={25} title='light mode'/> :
-                <MdDarkMode color={themeColors.icon} size={25} title='dark mode'/>
-              }
+              initial={{
+                opacity: 0,
+                rotate: 120,
+              }}
+              animate={{
+                opacity: 1,
+                rotate: 300,
+                transition: { duration: 1 },
+              }}
+              whileHover={{
+                opacity: 0.5,
+                rotate: 120,
+              }}
+              whileTap={{
+                opacity: 0.5,
+                rotate: 120,
+              }}
+              transition={{
+                duration: 0.3,
+                ease: "easeInOut",
+              }}
+              style={{ backgroundColor: "transparent" }}
+              onClick={() => handleTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme === "dark" ? (
+                <IoMdSunny
+                  color={themeColors.icon}
+                  size={25}
+                  title="light mode"
+                />
+              ) : (
+                <MdDarkMode
+                  color={themeColors.icon}
+                  size={25}
+                  title="dark mode"
+                />
+              )}
             </motion.button>
           </div>
-          {
-            window.innerWidth >= 1024 &&
-            <div style={{ height: 500, width: '100%', marginBottom: 20 }}> 
+          {window.innerWidth >= 1024 && (
+            <div style={{ height: 500, width: "100%", marginBottom: 20 }}>
               <GridMotion items={items} />
             </div>
-          }
-          
+          )}
 
-          <div 
-            className='menu-list gap-5 mt-2 mb-2 '
+          <div
+            className="menu-list gap-5 mt-2 mb-2 "
             style={{ color: themeColors.background }}
           >
             {menuItems.map((item, index) => (
-              <div key={index} className="menu-item" onClick={() => setSelected(item.title)}>
-                <div className='d-flex gap-2 align-items-center'>
+              <div
+                key={index}
+                className="menu-item"
+                onClick={() => setSelected(item.title)}
+              >
+                <div className="d-flex gap-2 align-items-center">
                   {item.icon}
-                  {
-                    width > 425 &&
-                    <p 
+                  {width > 425 && (
+                    <p
                       className={selected === item.title ? "active" : ""}
-                      style={{ color: 
-                        selected === item.title ? 
-                          (themeColors.text) : (
-                            'gray'
-                          )}}
-                    >{item.title}</p>
-                  }
-                  
+                      style={{
+                        color:
+                          selected === item.title ? themeColors.text : "gray",
+                      }}
+                    >
+                      {item.title}
+                    </p>
+                  )}
                 </div>
                 {selected == item.title && (
                   <motion.div
@@ -192,36 +343,39 @@ const ProjectsPage = () => {
           </div>
         </div>
 
-        <div className='projects-card-list'>
-          {
-            projectList.map((item, index) => (
-              <motion.div
-                initial={{ opacity: 0, y: 200 }}
-                whileInView={{ opacity: 1, y: 0, transition: { delay: parseFloat(`0.${index + 2}`) } }}
-                whileTap= {{ rotate: 10, transition: { type: 'spring', stiffness: 300} }}
-              >
-                <Card
-                  key={index}
-                  width='auto' 
-                  height='300px'
-                  title={item.title}
-                  description={item.description}
-                  thumbnail={item.thumbnail}
-                  icon={item.icon}
-                />
-              </motion.div>
-            ))
-          }
-          
-
+        <div className="projects-card-list">
+          {projectList.map((item, index) => (
+            <motion.div
+              initial={{ opacity: 0, y: 200 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: { delay: parseFloat(`0.${index + 2}`) },
+              }}
+              whileTap={{
+                rotate: 10,
+                transition: { type: "spring", stiffness: 300 },
+              }}
+            >
+              <Card
+                key={index}
+                width="auto"
+                height="300px"
+                title={item.title}
+                description={item.description}
+                thumbnail={item.thumbnail}
+                icon={item.icon}
+                repository={item.repository}
+              />
+            </motion.div>
+          ))}
         </div>
-   
       </div>
-      <footer className='footer'>
-        <Footer/>
+      <footer className="footer">
+        <Footer />
       </footer>
     </motion.div>
-  )
-}
+  );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
